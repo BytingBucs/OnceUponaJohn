c8cf0b4cf8ce468b6038deeaa8e169d8c2c544df
jtherrien1 <jtherrien1@bucs.fsw.edu>
2016-02-25 15:35:04 -0500

    Update

---
+++ Chapter4/Amazerfic/javascripts/app.js
@@ -0,0 +1,60 @@
+var main = function (toDoObjects) {
+    "use strict";
+
+    var toDos = toDoObjects.map(function (toDo) {
+          // we'll just return the description
+          // of this toDoObject
+          return toDo.description;
+    });
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+
+            } else if ($element.parent().is(":nth-child(3)")) {
+                var tags = [];
+
+                toDoObjects.forEach(function (toDo) {
+                    toDo.tags.forEach(function (tag) {
+                        if (tags.indexOf(tag) === -1) {
+                            tags.push(tag);
+                        }
+                    });
+                });
+                
+				$content = $("<div>").append($input).append($button);
+				/* Alternatively append() allows multiple arguments so the above 
+				can be done with $content = $("<div>").append($input, $button); */
+			}
+			
+			$("main .content").append($content);
+			
+			return false;
+			
+		});
+	});
+	
+	$(".tabs a:first-child span").trigger("click");
+};
+$(document).ready(main);
